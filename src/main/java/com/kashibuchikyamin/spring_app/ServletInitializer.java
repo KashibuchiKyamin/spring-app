dd58fbe78d295c153b4ba22f78ca4e39148e5588
kyamin <y.mrkm.0722@gmail.com>
2025-08-23 21:34:09 +0900

    Javadocコメント追加

--- src/main/java/com/kashibuchikyamin/spring_app/ServletInitializer.java
+++ src/main/java/com/kashibuchikyamin/spring_app/ServletInitializer.java
@@ -3,8 +3,18 @@ package com.kashibuchikyamin.spring_app;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+/**
+ * サーブレットコンテナ上でSpring Bootアプリケーションを初期化・構成するクラスです。
+ * 主にWARデプロイ時に使用されます。
+ */
 public class ServletInitializer extends SpringBootServletInitializer {
 
+	/**
+	 * Spring Bootアプリケーションの設定を行います。
+	 *
+	 * @param application SpringApplicationBuilderインスタンス
+	 * @return 設定済みのSpringApplicationBuilderインスタンス
+	 */
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(SpringAppApplication.class);
--- src/main/java/com/kashibuchikyamin/spring_app/SpringAppApplication.java
+++ src/main/java/com/kashibuchikyamin/spring_app/SpringAppApplication.java
@@ -3,9 +3,18 @@ package com.kashibuchikyamin.spring_app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Spring Bootアプリケーションのエントリーポイントとなるクラスです。
+ * アプリケーションの起動処理を担当します。
+ */
 @SpringBootApplication
 public class SpringAppApplication {
 
+	/**
+	 * アプリケーションを起動します。
+	 *
+	 * @param args コマンドライン引数
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAppApplication.class, args);
 	}
